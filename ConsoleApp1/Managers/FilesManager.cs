4dc198fae4f91b8b6737534dbbaca07ef497d8d8
XRicko <idims52@gmail.com>
2020-08-07 18:39:52 +0300

    some fixes

---
+++ ConsoleApp1/Managers/FilesManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class FilesManager : IShow
+    {
+        public FileModel file = new FileModel();
+        public List<string> Files { get; private set; }
+
+        public void GetFiles(string path)
+        {
+            Files = Directory.GetFiles(path).ToList();
+        }
+        public string GetTextFromFile()
+        {
+            string output = File.ReadAllText(file.Path);
+            return output;
+        }
+        public void WriteTextToFile(string text)
+        {
+            File.WriteAllText(file.Path, text);
+        }
+        public void Show()
+        {
+            Files.Sort();
+
+            foreach (var file in Files)
+            {
+                Console.WriteLine("\n" + Path.GetFileName(file));
+            }
+        }
+    }
+}
