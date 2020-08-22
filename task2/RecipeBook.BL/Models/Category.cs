d50a9b23fc0d3125aacdbb4632e774b5c8cbf8d5
XRicko <idims52@gmail.com>
2020-08-22 14:49:17 +0300

    fixed typo in naming

--- task2/RecipeBook.BL/Models/Category.cs
+++ task2/RecipeBook.BL/Models/Category.cs
@@ -2,14 +2,14 @@
 {
     public class Category : ModelBase
     {
-        public int? ParentID { get; }
+        public int? ParentId { get; }
         public string ParentName { get; }
 
         public Category(string name, int? parentId = null, string parentName = null) : base(name)
         {
             if (parentId != null && parentName != null)
             {
-                ParentID = parentId;
+                ParentId = parentId;
                 ParentName = parentName;
             }
         }
