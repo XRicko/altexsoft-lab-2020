d88d8aa098d12ea59f901a54954d2fae8d4c793b
XRicko <idims52@gmail.com>
2020-09-21 01:59:18 +0300

    cleanup

--- task4/RecipeBook.Core/Models/Ingredient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RecipeBook.BL.Models
-{
-    public class Ingredient : ModelBase
-    {
-        public Ingredient(string name) : base(name) { }
-    }
-}
--- task4/RecipeBook.Core/Models/RecipeIngredient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RecipeBook.BL.Models
-{
-    public class RecipeIngredient
-    {
-        public Ingredient Ingredient { get; set; }
-        public string Amount { get; set; }
-    }
-}
