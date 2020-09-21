8c5ea098d6a851132c2fe17d650c0b6afbff0e8c
XRicko <idims52@gmail.com>
2020-09-21 18:52:49 +0300

    optimazed query a little bit

--- task3/queries/Getting recipes.sql
+++ task3/queries/Getting recipes.sql
@@ -6,9 +6,9 @@ SELECT c.CategoryId, c.CategoryName, c.ParentId, Categories_CTE.main_id AS main_
 FROM Category c
 INNER JOIN Categories_CTE ON c.ParentId = Categories_CTE.CategoryId)
 
-SELECT r.RecipeName, c.CategoryName, i.IngredientName, ri.Amount
+SELECT r.RecipeName, c.CategoryName, c.CategoryId, i.IngredientName, ri.Amount
 FROM Recipe r
 INNER JOIN RecipeIngredient ri ON r.RecipeId = ri.RecipeId
 INNER JOIN Category c ON r.CategoryId = c.CategoryId
 INNER JOIN Ingredient i ON ri.IngredientId = i.IngredientId
-WHERE r.RecipeName IN (SELECT TOP 3 r1.RecipeName FROM Recipe r1 WHERE r1.CategoryId IN (SELECT Categories_CTE.CategoryId FROM Categories_CTE WHERE main_id = 3))
+WHERE r.RecipeId IN (SELECT TOP 3 r1.RecipeId FROM Recipe r1 INNER JOIN Categories_CTE cte ON r1.CategoryId = cte.CategoryId AND cte.main_id = 3)
