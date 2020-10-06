e73657d8ced08b352287482027a9abc92d37acca
XRicko <idims52@gmail.com>
2020-10-06 19:58:50 +0300

    custom exceptions fixes

--- task5/RecipeBook.Core/Exceptions/ImpossibleDurationException.cs
+++ task5/RecipeBook.Core/Exceptions/ImpossibleDurationException.cs
@@ -4,7 +4,15 @@ namespace RecipeBook.Core.Exceptions
 {
     class ImpossibleDurationException : Exception
     {
+        public ImpossibleDurationException() { }
+
+        public ImpossibleDurationException(string message)
+            : base(message) { }
+
+        public ImpossibleDurationException(string message, Exception inner)
+            : base(message, inner) { }
+
         public ImpossibleDurationException(double duration)
-            : base($"Duration should be greater than 0. {duration} given instead") { }
+            : this($"Duration should be greater than 0. {duration} given instead") { }
     }
 }
