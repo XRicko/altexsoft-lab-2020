c0370efdc05bdde19be46c62eceb8331c8e7394f
XRicko <idims52@gmail.com>
2020-10-21 15:53:09 +0300

    add tests for StandardizeName extension

---
+++ task5/tests/RecipeBook.UnitTests/RecipeBook.Core.Tests/NameTransformingExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using RecipeBook.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RecipeBook.Core.Tests
+{
+    public class NameTransformingExtensionsTests
+    {
+        [Fact]
+        public void StandardizeName_ShouldReturnStringToTitleCase()
+        {
+            // Assert && Act
+            Assert.Equal("Something Interesting", " somEthing interesting   ".StandardizeName());
+        }
+
+        [Fact]
+        public void StandardizeName_ShouldReturnNull()
+        {
+            Assert.Null(" ".StandardizeName());
+        }
+    }
+}
